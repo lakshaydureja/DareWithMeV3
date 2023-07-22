76c8690703d9ddb41fa865a689dfbe540f59a47c
Lakshay Dureja <lakshaydureja7@gmail.com>
2023-07-22 16:07:22 +0530

    4

--- public/random/playscript2.js
+++ public/random/playscript2.js
@@ -51,160 +51,7 @@ socket.on('UserFound', (data) => {
 
             });
 
-        peer.on("open", function () {
-            console.log("Room created with user ID:", GlobalRoomCode);
-
-            peer.on("connection", function (conn) {
-
-                conn.on("open", function () {
-                    console.log("Connection established with", conn.peer);
-
-                    peer.on("call", function (call) {
-                        // Answer the incoming call
-                        call.answer(localStream);
-
-                        // Display remote video stream
-                        const remoteVideo = document.getElementById("remoteVideo");
-                        call.on("stream", function (remoteStream) {
-                            remoteVideo.srcObject = remoteStream;
-                        });
-                    });
-
-
-
-
-
-                    // Send a message
-                    const sendMessage = function (message) {
-                        conn.send({ type: "message", message: message });
-                    };
-
-                    // Send angle and turn data
-                    const sendAngleTurnData = function (angle, turn) {
-                        conn.send({ type: "angleTurnData", angle: angle, turn: turn });
-                    };
-
-                    // Handle data received from the other peer
-                    conn.on("data", function (data) {
-                        // Handle received data
-                        if (data.type === "message") {
-                            // Handle message
-                            const receivedMessage = data.message;
-                            recieveMsg(receivedMessage);
-                            console.log("Received message:", receivedMessage);
-
-                            // Send response back to the other peer
-                            // conn.send({ type: "response", message: "Response from the receiver" });
-                        } else if (data.type === "angleTurnData") {
-                            // Handle angle and turn data
-                            const receivedAngle = data.angle;
-                            const receivedTurn = data.turn;
-                            console.log("Received angle:", receivedAngle);
-                            console.log("Received turn:", receivedTurn);
-
-                            spin(receivedAngle);
-                            turn = receivedTurn;
-                            remoteVideo.classList.remove("turn");
-                            localVideo.classList.add("turn");
-
-                            // Send response back to the other peer
-                            // conn.send({ type: "response", message: "Response from the receiver" });
-                        }
-                    });
-
-
-                    //message part
-                    const messageList = document.getElementById('message-list');
-                    const messageInput = document.getElementById('msg');
-                    const sendButton = document.getElementById('send-button');
-
-
-
-                    //add and send message 
-                    function addMessage() {
-                        const messageText = messageInput.value;
-
-                        if (messageText.trim() === '') {
-                            return; // Ignore empty messages
-                        }
-
-                        const messageElement = document.createElement('li');
-                        messageElement.textContent = messageText;
-
-                        messageElement.classList.add('sender');
-
-                        sendMessage(messageText);
-
-
-                        messageList.appendChild(messageElement);
-
-                        messageInput.value = '';
-                        scrollToBottom(); // Scroll to bottom after appending the message
-
-                    }
-                    //recive msg and add it
-
-                    function recieveMsg(msgx) {
-                        const messageElement = document.createElement('li');
-                        messageElement.textContent = msgx;
-                        messageElement.classList.add('receiver');
-                        messageList.appendChild(messageElement);
-                        scrollToBottom();
-                    }
-
-
-                    function scrollToBottom() {
-                        var messageScreen = document.querySelector('.message-screen');
-                        messageScreen.scrollTop = messageScreen.scrollHeight;
-                    }
-
-
-
-                    sendButton.addEventListener('click', () => {
-                        addMessage();
-
-                    });
-
-                    messageInput.addEventListener('keydown', (event) => {
-                        if (event.key === 'Enter') {
-
-                            addMessage();
-                        }
-                    });
-
-
-
-
-                    // angle turn part
-                    spinbtn.addEventListener("click", function () {
-
-
-                        if (turn == 0) {
-                            angle = 10 * 360 + generateRandomAngle();
-                            console.log("angle2", angle);
-                            sendAngleTurnData(angle, 1);
-                            spin(angle);
-                            turn = 1;
-                            localVideo.classList.remove("turn");
-                            remoteVideo.classList.add("turn");
-
-                        } else if (turn == 1) {
-                            showModalPopup("Wait!!!👀 It's not your Turn");
-
-                            console.log("its not your turn");
-                        } else {
-                            console.log("Some Error");
-                        }
-
-
-                    });
-                });
-
-
-            });
-
-
-        });
+       
 
 
 
@@ -533,3 +380,159 @@ function generateRandomAngle() {
 
     return finalAngle;
 }
+
+//?----------------------------test-----------------
+peer.on("open", function () {
+    console.log("Room created with user ID:", GlobalRoomCode);
+
+    peer.on("connection", function (conn) {
+
+        conn.on("open", function () {
+            console.log("Connection established with", conn.peer);
+
+            peer.on("call", function (call) {
+                // Answer the incoming call
+                call.answer(localStream);
+
+                // Display remote video stream
+                const remoteVideo = document.getElementById("remoteVideo");
+                call.on("stream", function (remoteStream) {
+                    remoteVideo.srcObject = remoteStream;
+                });
+            });
+
+
+
+
+
+            // Send a message
+            const sendMessage = function (message) {
+                conn.send({ type: "message", message: message });
+            };
+
+            // Send angle and turn data
+            const sendAngleTurnData = function (angle, turn) {
+                conn.send({ type: "angleTurnData", angle: angle, turn: turn });
+            };
+
+            // Handle data received from the other peer
+            conn.on("data", function (data) {
+                // Handle received data
+                if (data.type === "message") {
+                    // Handle message
+                    const receivedMessage = data.message;
+                    recieveMsg(receivedMessage);
+                    console.log("Received message:", receivedMessage);
+
+                    // Send response back to the other peer
+                    // conn.send({ type: "response", message: "Response from the receiver" });
+                } else if (data.type === "angleTurnData") {
+                    // Handle angle and turn data
+                    const receivedAngle = data.angle;
+                    const receivedTurn = data.turn;
+                    console.log("Received angle:", receivedAngle);
+                    console.log("Received turn:", receivedTurn);
+
+                    spin(receivedAngle);
+                    turn = receivedTurn;
+                    remoteVideo.classList.remove("turn");
+                    localVideo.classList.add("turn");
+
+                    // Send response back to the other peer
+                    // conn.send({ type: "response", message: "Response from the receiver" });
+                }
+            });
+
+
+            //message part
+            const messageList = document.getElementById('message-list');
+            const messageInput = document.getElementById('msg');
+            const sendButton = document.getElementById('send-button');
+
+
+
+            //add and send message 
+            function addMessage() {
+                const messageText = messageInput.value;
+
+                if (messageText.trim() === '') {
+                    return; // Ignore empty messages
+                }
+
+                const messageElement = document.createElement('li');
+                messageElement.textContent = messageText;
+
+                messageElement.classList.add('sender');
+
+                sendMessage(messageText);
+
+
+                messageList.appendChild(messageElement);
+
+                messageInput.value = '';
+                scrollToBottom(); // Scroll to bottom after appending the message
+
+            }
+            //recive msg and add it
+
+            function recieveMsg(msgx) {
+                const messageElement = document.createElement('li');
+                messageElement.textContent = msgx;
+                messageElement.classList.add('receiver');
+                messageList.appendChild(messageElement);
+                scrollToBottom();
+            }
+
+
+            function scrollToBottom() {
+                var messageScreen = document.querySelector('.message-screen');
+                messageScreen.scrollTop = messageScreen.scrollHeight;
+            }
+
+
+
+            sendButton.addEventListener('click', () => {
+                addMessage();
+
+            });
+
+            messageInput.addEventListener('keydown', (event) => {
+                if (event.key === 'Enter') {
+
+                    addMessage();
+                }
+            });
+
+
+
+
+            // angle turn part
+            spinbtn.addEventListener("click", function () {
+
+
+                if (turn == 0) {
+                    angle = 10 * 360 + generateRandomAngle();
+                    console.log("angle2", angle);
+                    sendAngleTurnData(angle, 1);
+                    spin(angle);
+                    turn = 1;
+                    localVideo.classList.remove("turn");
+                    remoteVideo.classList.add("turn");
+
+                } else if (turn == 1) {
+                    showModalPopup("Wait!!!👀 It's not your Turn");
+
+                    console.log("its not your turn");
+                } else {
+                    console.log("Some Error");
+                }
+
+
+            });
+        });
+
+
+    });
+
+
+});
